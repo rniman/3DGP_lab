2031ce67ca97d9076f9e43839ee35f525c7c78f3
rniman <105342620+rniman@users.noreply.github.com>
2023-03-29 00:23:52 +0900

    #define 방향 수정

--- LabProject02/stdafx.h
+++ LabProject02/stdafx.h
@@ -34,12 +34,12 @@ using namespace DirectX::PackedVector;
 constexpr unsigned int FRAMEBUFFER_WIDTH{ 640 };
 constexpr unsigned int FRAMEBUFFER_HEIGHT{ 480 };
 
-#define DIR_FORWARD				0x01
-#define DIR_BACKWARD			0x02
-#define DIR_LEFT				0x04
-#define DIR_RIGHT				0x08
-#define DIR_UP					0x10
-#define DIR_DOWN				0x20
+constexpr DWORD DIR_FORWARD{ 0x01 };
+constexpr DWORD DIR_BACKWARD{ 0x02 };
+constexpr DWORD DIR_LEFT{ 0x04 };
+constexpr DWORD DIR_RIGHT{ 0x08 };
+constexpr DWORD DIR_UP{ 0x10 };
+constexpr DWORD DIR_DOWN{ 0x20 };
 
 constexpr float EPSILON{ 1.0e-6f };
 
